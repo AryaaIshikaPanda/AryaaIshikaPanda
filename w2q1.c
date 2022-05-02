2131944b514ed7b78e3a42081af35ee25d22bb4e
Aryaa Ishika Panda <99532150+AryaaIshikaPanda@users.noreply.github.com>
2022-05-02 21:32:46 +0530

    Add files via upload

---
+++ w2q1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include <string.h>
+int main()
+{
+    char s1[100],s2[100];  
+    int i,j,k,len=0,temp;
+ 
+    printf("Enter  string1: ");
+    scanf("%s",&s1);
+    printf("Enter  string2: ");
+    scanf("%s",&s2);
+    j=strlen(s1);
+    
+    for(i=0;s2[i]!='\0';i++)  
+    {
+    	s1[i+j]=s2[i];
+     }
+    s1[i+j]='\0';
+ 
+    printf("combined two strings =%s\n",s1);
+    for (k = 0; k<len/2; k++)  
+    {   
+        temp = s1[k];  
+        s1[k] = s1[len - k - 1];  
+        s1[len - k - 1] = temp;  
+    } 
+    printf("reverse string= %s\n",s1);
+    return 0;
+}
